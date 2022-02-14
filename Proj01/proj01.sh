e48e09709a648514300168b4a8b7d8dc4f303958
wit4401 <wtownsend2@gulls.salisbury.edu>
2022-02-14 13:16:43 -0500

    update

--- Proj01/proj01.sh
+++ Proj01/proj01.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+contin=yes
+file=save.text
+
+function insert{
+}
+
+function delete{
+}
+
+function modify{
+}
+
+function retrieve{
+}
+
+while [$contin -eq "yes"]
+do
+    option=0;
+    read $option
+    if [$option -eq "X"]
+        contin=no
+done
+
+exit 0 
