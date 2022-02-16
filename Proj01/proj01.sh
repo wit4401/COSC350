340dc3e02fec95797f789a0816538baa67fb8b47
wit4401 <wtownsend2@gulls.salisbury.edu>
2022-02-16 11:52:33 -0500

    update

--- Proj01/proj01.sh
+++ Proj01/proj01.sh
@@ -2,14 +2,17 @@
 file=save.text
 option='A'
 
+echo '-------------------------------'
 function DisplayMenu {
-    echo '----------------------------------------------------------------------------------------'
-    echo 'A'
-    echo 'B'
-    echo 'C'
-    echo 'D'
-    echo 'E'
-    echo 'X'
+    echo 'Employee Database Simulator'
+    echo '-------------------------------'
+    echo 'A. Insert'
+    echo 'B. Modify'
+    echo 'C. Delete'
+    echo 'D. Retrieve'
+    echo 'E. Display'
+    echo 'X. Exit Program'
+    echo -n 'Please Select an Option: '
 }
 
 function insert {
@@ -32,22 +35,11 @@ function printDatabase {
     echo 'shit'
 }
 
-while [ $option == 'A' ] || [ $option == 'B' ] || [ $option == 'C' ] || [ $option == 'D' ] || [ $option == 'E' ]
+while [ $option != 'X' ]
 do
-    option='N'
     DisplayMenu
-    read $option
-    if [ $option == 'A' ]; then
-        insert
-    elif [ $option == 'B' ]; then
-        modify
-    elif [ $option == 'C' ]; then
-        delete
-    elif [ $option == 'D' ]; then
-        retrieve
-    elif [ $option == 'E' ]; then
-        display
-    fi
+    read option
+    echo '-------------------------------'
 done
 
 exit 0 
