f43a64e3918779a891997148d7289a6c9054f8a4
wit4401 <wtownsend2@gulls.salisbury.edu>
2022-02-18 00:03:27 -0500

    lab03

--- Lab03/task6a.sh
+++ Lab03/task6a.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+base=0
+num=0
+
+while [ $base -lt 4 ] || [ $num -eq 0 ];do
+    echo -n "Base Value: "
+    read base
+    num=`expr $base % 2`
+    if [ $base -lt 4 ];then
+        echo "Base Value too small!"
+    elif  [ $num -eq 0 ];then
+        echo "Base Value must be odd!"
+    fi
+done
+
+exit 0
--- Lab03/task6b.sh
+++ Lab03/task6b.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+base=0
+num=0
+tail=0
+
+while [ $base -lt 4 ] || [ $num -eq 0 ];do
+    echo -n "Base Value: "
+    read base
+    num=`expr $base % 2`
+    if [ $base -lt 4 ];then
+        echo "Base Value too small!"
+    elif  [ $num -eq 0 ];then
+        echo "Base Value must be odd!"
+    fi
+done
+
+while [ $tail -gt 0 ];do
+    echo -n "Tail value: "
+    read tail
+    if [ $tail -le 0 ];then
+        echo "Tail value must be positive!"
+done
+
+exit 0
