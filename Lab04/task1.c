2a03a9745de5aafe705f0d70717ce6a9016a31a2
William Isaiah Townsend <wtownsend2@hpcl3-5.salisbury.edu>
2022-03-10 12:42:15 -0500

    Lab04&Midterm

--- Lab04/task1.c
+++ Lab04/task1.c
@@ -1,3 +1,50 @@
-#include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
 #include<unistd.h>
+#include<fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int intArrToInt(int digits[], int length){
+    int finalNum=0;
+    for(int i=0;i<length;i++){
+        finalNum = finalNum * 10 + digits[i];
+    }
+    return finalNum;
+}
+
+int main(int argc, char *argv[]){
+    //error checks the arguements    
+    if(argc!=3){
+        write(2,"Error! Please pass two arguements.\n",36);
+        exit(2);
+    }
+    
+    umask(000);//allows permissions to be set to any for usr, grp, oth
+    int fileIn=open(argv[1],O_RDONLY);//opens input file determined by the first arguement
+    int fileOut=open(argv[2],O_CREAT | O_RDWR, 0666);//creates the output file (name determined by second command line arguement
+    
+    //error check for files
+    if(fileIn==-1 || fileOut==-1){
+        write(2,"Error! Cannot open file.\n",26);
+        exit(1);  
+    }
+    
+    char b;
+    int rbyte;
+    int *num;
+    int index=0;
+    while((rbyte=read(fileIn,&b,1))>0){
+        if(b != ' ')
+            num[index++]= b - '0';
+        else{
+           //printf("%d ",intArrToInt(num,index+1));
+            char newB = (char) intArrToInt(num,index);
+            write(fileOut,&newB,1);
+            index=0;
+        }
+    }
+    close(fileIn);
+    close(fileOut);
+    exit(0);
+}
--- Lab04/task2.c
+++ Lab04/task2.c
@@ -1,3 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<fcntl.h>
 #include<unistd.h>
+#include<ctype.h>
+#include<string.h>
+
+/************************************************
+Convert integer to string
+Params: x is the int to be converted,
+        str is the string into which to write
+Returns: length of the string
+**************************************************/
+int convIntToStr(char * str, int x){
+    sprintf(str, "%d", x);
+    return (strlen(str));
+}
+
+int main(int argc,char *argv[]){
+    if (argc!=2){
+        puts("Must pass ONE arguement!");
+        exit(1);
+    }
+    int file;
+    if (file=open(argv[1],O_RDONLY)==-1){
+        puts("Error could not open file!");
+        exit(2);
+    }
+    char *b;
+    char readByte;
+    int rbyte;
+    int index=0;
+    
+    while((rbyte=read(file,&readByte,1))>0)
+        b[index++]=readByte;
+    
+    int num=0;
+    for(int i=0;i<index;i++)
+        num = num * 10 + b[i] - '0';
+    printf("%d",num);
+    exit(0);
+}
