bbe8fac38fe28198493bf1a23f59f9c74678c823
wit4401 <wtownsend2@gulls.salisbury.edu>
2022-03-11 12:13:18 -0500

    Lab04

--- Lab04/MyMv.c
+++ Lab04/MyMv.c
@@ -5,12 +5,27 @@
 *********************/
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<unistd.h>
 #include<fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
 int main(int argc,char *argv[]){
-    
+    struct stat fileType;
+    if (stat(argv[2],fileType)==-1){
+        puts("lstat Error");
+        exit(1);
+    }
+    if(S_ISDIR(fileType.st_mode)){
+        
+    }
+    else if(S_ISREG(fileType.st_mode)){
+        
+    }
+    else{
+        puts("Unknown Error");
+        exit(2);
+    }
     exit(0);
 }
