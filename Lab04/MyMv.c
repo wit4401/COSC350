4e5ebae2d16693d941afe52eb0cef5d982015e98
William Isaiah Townsend <wtownsend2@hs101-8.salisbury.edu>
2022-03-14 14:55:53 -0500

    Lab04

--- Lab04/MyMv.c
+++ Lab04/MyMv.c
@@ -12,16 +12,39 @@
 #include <sys/stat.h>
 
 int main(int argc,char *argv[]){
-    struct stat fileType;
-    if (stat(argv[2],fileType)==-1){
-        puts("lstat Error");
+    if(argc!=3){
+        puts("Must pass two arguements!");
         exit(1);
     }
+    
+    struct stat fileType;
+    char *pwd="/mnt/linuxlab/home";
+    char *pathTo=argv[2];
+    char oldPath=argv[1];
+    if (pathTo[0]=='~'){
+        for(int i=0;i<strlen(pathTo)-1;i++)
+            pathTo[i]=pathTo[i+1];
+        strcat(pwd,pathTo);
+    }
+    strcat("/COSC350/",oldPath);
+    strcat(pwd,oldPath);
+    printf("Old Path: %s\n",oldPath);
+    printf("New Path: %s\n",pathTo)
+    if (stat(pathTo,&fileType)==-1){
+        puts("lstat Error");
+        exit(2);
+    }
     if(S_ISDIR(fileType.st_mode)){
-        
+        char *newPath;
+        strcpy(argv[1],newPath);
+        strcat('/',newPath);
+        srtcat(path,newPath);
+        link(newPath,oldPath);
+        unlink(oldPath);
     }
     else if(S_ISREG(fileType.st_mode)){
-        
+        link(oldPath,newPath);
+        unlink(oldPath);
     }
     else{
         puts("Unknown Error");
