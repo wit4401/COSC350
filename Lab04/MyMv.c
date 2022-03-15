828c45d40ea7d271dde42041338c67840dcb0d49
wit4401 <wtownsend2@gulls.salisbury.edu>
2022-03-14 22:38:08 -0400

    Lab04

--- Lab04/MyMv.c
+++ Lab04/MyMv.c
@@ -24,7 +24,10 @@ int main(int argc,char *argv[]){
     
     strcat(oldPath,pwd);
     strcpy(newPath,argv[2]);
-
+    if(open(argv[1],O_RDONLY)==-1){
+        puts("File doesn't exist.");        
+        exit(2);
+    }
     if (stat(newPath,&fileType)==-1){
         int len=strlen(newPath)-1;
         int newNameLen;
@@ -63,12 +66,49 @@ int main(int argc,char *argv[]){
             if (unlink(oldPath)==-1){printf("Error\n");}
         }
         else if(S_ISREG(fileType.st_mode)){
-            puts("File name exists!");
-            exit(1);
+            char confir='m';
+            while(confir!='n' && confir!='y'){         
+                puts("\nA file with that name exists.");
+                printf("Overwrite File? (y/n): ");
+                scanf(" %c",&confir);
+                if(confir!='n' && confir!='y')
+                    puts("Invalid Input!");
+                if(confir=='n')
+                    exit(3);
+            }
+            puts("");            
+            unlink(newPath);
+            int len=strlen(newPath)-1;
+            int newNameLen;
+            char newOld[1024];
+        
+            while(newPath[len--]!='/')
+                newNameLen++;
+        
+            char rev[newNameLen];
+            char newName[newNameLen];
+            int index=newNameLen;
+            len=strlen(newPath);
+            int lenI=len-1;
+        
+            for(int i=0;i<len;i++)
+                rev[i]=newPath[lenI--];
+        
+            len=index;
+            for(int i=0;i<len;i++)
+                newName[i]=rev[--index];
+        
+            strcat(newOld,oldPath);
+            strcat(oldPath,argv[1]);
+            strcat(newOld,newName);
+        
+            rename(oldPath,newOld);
+            link(oldPath,newPath);
+            unlink(oldPath);
         }
         else{
             puts("Unknown Error");
-            exit(2);
+            exit(4);
         }
     }
     exit(0);
