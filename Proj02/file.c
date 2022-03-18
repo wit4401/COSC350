694f772aef8bce9f265c1fe2fa758e4f01cf0774
William Townsend <88016093+wit4401@users.noreply.github.com>
2022-03-18 00:26:59 -0400

    Add files via upload

---
+++ Proj02/file.c
@@ -0,0 +1,14 @@
+/****************
+|   Project 2   |
+| Will Townsend |
+****************/
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+
+int main(){
+	
+}
