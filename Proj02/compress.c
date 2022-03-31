eefab9721f9122b246a3303a693234e1a7dfb5a9
wit4401 <wtownsend2@gulls.salisbury.edu>
2022-03-31 11:58:55 -0400

    Added Freq Arr

--- Proj02/compress.c
+++ Proj02/compress.c
@@ -5,6 +5,42 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 
-int main(){
-	
+struct pair{
+    int freq;
+    char val;
+};
+
+int main(int argc,char *argv[]){
+	if(argc!=2){
+        printf("Error!\n");
+        exit(1);
+    }
+    int file=open(argv[1],O_RDONLY);
+    if (file<0){
+        printf("Open Error!\n");
+        exit(2);
+    }
+    int rbyte;
+    char b;
+    struct pair *list;
+    int listLen=1;
+    read(file,&b,1);
+    list[0].val=b;
+    list[0].freq=1;
+    while((rbyte=read(file,&b,1))>0){
+        int add=1;
+        for(int i=0;i<listLen;i++){
+            if(list[i].val==b){
+                list[i].freq++;
+                add=0;
+            }
+        }
+        if(add){
+            list[listLen].val=b;
+            list[listLen++].freq=1;
+        }
+    }
+    for(int i=0;i<listLen;i++)
+        printf("Value: '%c'\nFrequency: %d\n",list[i].val,list[i].freq);
+    exit(0);
 }
--- Proj02/huffman.h
+++ Proj02/huffman.h
@@ -1,7 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct pair{
+    int freq;
+    char val;
+};
+
 struct node{
 	struct node *left;
 	struct node *right;
+    struct pair info;
 };
