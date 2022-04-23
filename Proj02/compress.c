0a7fd6225b7b9d697c65231be0edd12c076b4d77
wit4401 <wtownsend2@gulls.salisbury.edu>
2022-04-23 18:21:53 -0400

    Proj02 Update

--- Proj02/compress.c
+++ Proj02/compress.c
@@ -4,57 +4,7 @@
 #include<fcntl.h>
 #include<sys/stat.h>
 #include<sys/types.h>
-
-struct pair{
-    int freq;
-    char val;
-};
-
-struct qNode{
-    struct pair info;
-    struct qNode *next;
-};
-
-void printQueue(struct qNode *start){
-    struct qNode *curr=start;
-    while(curr!=NULL){
-        printf("%c:%d\n",curr->info.val,curr->info.freq);
-        curr=curr->next;
-    }
-}
-
-struct qNode *newPair(struct pair newInfo){
-    struct qNode *newNode=malloc(sizeof(struct qNode));
-    newNode->next=NULL;
-    newNode->info=newInfo;
-    return newNode;
-}
-
-//This function frees the memory allocated in the priority queue
-void pop(struct qNode **start){
-    struct qNode *pop=(*start);
-    (*start)=(*start)->next;
-    free(pop);
-}
-
-/*
- * We must utilize the address of the pair structure instead of storing it into another
- * temporary variable to be returned. This will modify it directly which is what is
- * needed to build priority queue which is required to build the huffman coding tree
- */
-void push(struct qNode **start,struct qNode *newNode){
-    if((*start)==NULL || newNode->info.freq < (*start)->info.freq){
-        newNode->next=(*start);
-        (*start)=newNode;
-    }
-    else{
-        struct qNode *curr=(*start);
-        while(curr->next != NULL && curr->next->info.freq < newNode->info.freq)
-            curr=curr->next;
-        newNode->next = curr->next;
-        curr->next=newNode;
-    }
-}
+#include"huffman.h"
 
 int main(int argc,char *argv[]){
     //checks that an arguement has been passed 
--- Proj02/huffman.c
+++ Proj02/huffman.c
@@ -8,23 +8,67 @@
 struct pair{
     int freq;
     char val;
-    struct pair *next;
 };
 
-struct node{
+struct qNode{
+    struct pair info;
+    struct qNode *next;
+};
+
+void printQueue(struct qNode *start){
+    struct qNode *curr=start;
+    while(curr!=NULL){
+        printf("%c:%d\n",curr->info.val,curr->info.freq);
+        curr=curr->next;
+    }
+}
+
+struct qNode *newPair(struct pair newInfo){
+    struct qNode *newNode=malloc(sizeof(struct qNode));
+    newNode->next=NULL;
+    newNode->info=newInfo;
+    return newNode;
+}
+
+//This function frees the memory allocated in the priority queue
+void pop(struct qNode **start){
+    struct qNode *pop=(*start);
+    (*start)=(*start)->next;
+    free(pop);
+}
+
+//We must utilize the address of the pair structure instead of storing it into another
+//temporary variable to be returned. This will modify it directly which is what is
+//needed to build priority queue which is required to build the huffman coding tree 
+void push(struct qNode **start,struct qNode *newNode){
+    if(newNode->info.freq < (*start)->info.freq){
+        newNode->next=(*start);
+        (*start)=newNode;
+    }
+    else{
+        struct qNode *curr=(*start);
+        while(curr->next != NULL && curr->next->info.freq < newNode->info.freq)
+            curr=curr->next;
+        newNode->next = curr->next;
+        curr->next=newNode;
+    }
+}
+
+struct treeNode{
     struct node *parent;
     struct node *left;
     struct node *right;
     struct pair info;
 };
 
-node *creatHuffTree(struct pair *list, int len){
+struct treeNode *creatHuffTree(struct pair *list, int len){
+    return NULL;
 }
 
-void compress(int fd,struct huffNode *tree){
+void compress(int fd,struct treeNode *tree){
 }
 
-void uncompress(int fd,struct huffNode *tree){
+void uncompress(int fd,struct treeNode *tree){
 }
 
 void pairPush(){
