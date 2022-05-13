ddc2c3b5e073949074b02042b27ff399236e3193
William Isaiah Townsend <wtownsend4401@gmail.com>
2022-05-13 15:17:40 -0500

    modified question1 to task command line arguements instead

--- midterm3/question1/parent.c
+++ midterm3/question1/parent.c
@@ -6,45 +6,23 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 
-int numLen(int val){
-	int len=0;
-	while(val!=0){
-		len++;
-		val/=10;
-	}
-	return len;
-}
-
-int main(){
-	int num1,num2;
-	scanf("%d",&num1);
-	scanf("%d",&num2);
+int main(int argc,char *argv[]){
 	pid_t pid;
 	pid=fork();
-	char *str1=malloc(numLen(num1)*sizeof(char));
-	char *str2=malloc(numLen(num2)*sizeof(char));
 	if(pid==-1){
 		puts("fork() error");
 		exit(1);
 	}
-	else if(pid==0){
-		sprintf(str1,"%d",num1);
-		sprintf(str2,"%d",num2);
-		execl("/mnt/linuxlab/home/wtownsend2/COSC350/midterm3/question1/child","./child","+",str1,str2,NULL);
-	}
+	else if(pid==0)
+		execl("/mnt/linuxlab/home/wtownsend2/COSC350/midterm3/question1/child","./child","+",argv[1],argv[2],NULL);
 
 	pid_t pid2=fork();
-	char *str3=malloc(numLen(num1)*sizeof(char));
-	char *str4=malloc(numLen(num2)*sizeof(char));
 	if(pid2==-1){
 		puts("fork() error");
 		exit(1);
 	}
-	else if(pid2==0){
-		sprintf(str3,"%d",num1);
-		sprintf(str4,"%d",num2);
-		execl("/mnt/linuxlab/home/wtownsend2/COSC350/midterm3/question1/child","./child","-",str3,str4,NULL);
-	}
+	else if(pid2==0)
+		execl("/mnt/linuxlab/home/wtownsend2/COSC350/midterm3/question1/child","./child","-",argv[1],argv[2],NULL);
 	else{
 		waitpid(pid2,NULL,0);
 		exit(0);
